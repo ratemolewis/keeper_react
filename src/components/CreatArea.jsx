86e021048f96abc524f8e75e8947dbb897fecef7
Ratemo Lewis <retemolewis@gmail.com>
2023-06-11 16:14:14 +0300

    and Material UI  compoments

--- src/components/CreatArea.jsx
+++ src/components/CreatArea.jsx
@@ -1,13 +1,18 @@
 import React, { useState } from "react";
+import AddIcon from '@mui/icons-material/Add';
+import Fab from '@mui/material/Fab';
+import Zoom from '@mui/material/Zoom';
 
 function CreatArea(props){
  const [note, setNote] = useState({
     title:"",
     content:""
  })
+ const [isEnabled, setIsEnabled] = useState(false);
 
  function handleChange(event){
  const { name, value } = event.target;
+
 
  setNote(prevNote => {
     return {
@@ -31,9 +36,13 @@ function CreatArea(props){
     return (
         <div>
         <form className="create-note">
-            <input name="title" onChange={handleChange} value={note.title} placeholder="Title" />
+            isEnabled ? <input name="title" onChange={handleChange} value={note.title} placeholder="Title" /> : null
             <textarea name="content" onChange={handleChange} value={note.content} placeholder="Take a note ..." rows="3"/>
-            <button onClick={submitNote}>Add</button>
+            //zoom, enables zoom in/Animation
+            <Zoom in={true}>
+            //floating action button/Fab, enables hover color change
+            <Fab onClick={submitNote}><AddIcon ></AddIcon></Fab>
+            </Zoom>
         </form>
         </div>
     );
