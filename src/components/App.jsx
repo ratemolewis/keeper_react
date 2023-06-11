252c6109b6d3c6c7b2459fb86486269ac000058a
Ratemo Lewis <retemolewis@gmail.com>
2023-06-11 12:18:22 +0300

    add new note functionality to array notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,38 @@
-import react from "react";
+import react, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
-
-function noteEntry(note){
-    return <Note 
-         key={note.id}
-         title={note.title}
-         content={note.content}
-    />
-}
+import CreatArea from "./CreatArea";
 
 function App(){
+    //use state to track the changing array
+const [notes, setNote] =  useState([]);
+
+    function addNote(newNote){
+        setNote(preNote => {
+            // set the new note to the array with previous items and return it
+           return [...preNote,newNote];
+        });
+        
+        }
+
     return <div>
-         <Header />
+        <Header />
+        <CreatArea
+        onAdd={addNote}
+         />
+         {notes.map((noteItem, index) =>{
+            /* each of the new noteItem, return a note component */
+            return <Note 
+                keys={index}
+                id={index}
+                title={noteItem.title}
+                content={noteItem.content}
+            />
+            
+         })}
          <Footer />
-         {notes.map(noteEntry)}
+        
     </div>
 }
 
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
-function Note(prop){
-    return <div className="note">
-        <h1>{prop.title}</h1>
-        <p>{prop.content}</p>
+function Note(props) {
+  function handleDelete(){
+    onDelete(props.id);
+  }  
+  return (
+    <div className="note">
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+      <button onClick={handleDelete}>DELETE</button>
     </div>
+  );
 }
 
 export default Note;
